1901cf374365b29cf2a8aba4b7042e0dc27e58c8
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-03 13:24:32 +0900

    自分のボードに対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
+
 end
