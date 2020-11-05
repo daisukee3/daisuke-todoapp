3c784fc7a2482c4e19f56dde320681c95850edb1
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-05 11:31:59 +0900

    コード間違い修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,11 +23,11 @@ class BoardsController < ApplicationController
     end
 
     def edit
-        @board = current_user.boards.find(params{:id})
+        @board = current_user.boards.find(params[:id])
     end
 
     def update
-        @board = current_user.boards.find(params{:id})
+        @board = current_user.boards.find(params[:id])
         if @board.update(board_params)
             redirect_to board_path(@board), notice: '更新出来ました'
         else
