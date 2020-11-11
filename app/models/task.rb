ee163748a888960190ce15748eeac3e576a9ebd1
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-11 20:00:52 +0900

    commentモデルを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -20,5 +20,6 @@ class Task < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    has_many :comments, dependent: destroy
     belongs_to :user
 end
