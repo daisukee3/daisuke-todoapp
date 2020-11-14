bf48b9d13df86dc69427e07df205552dfe226cab
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-14 18:41:46 +0900

    コード修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,10 +16,10 @@
 #
 class Task < ApplicationRecord
     has_one_attached :eyecatch
-    
+
     validates :title, presence: true
     validates :content, presence: true
 
-    has_many :comments, dependent: destroy
+    has_many :comments, dependent: :destroy
     belongs_to :user
 end
