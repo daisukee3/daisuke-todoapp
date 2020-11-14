fb7ca70f787ab9d0bc489669999670a9f9a3a6ec
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-14 19:44:21 +0900

    createアクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,22 @@ class CommentsController < ApplicationController
         task = Task.find(params[:task_id])
         @comment = task.comments.build
     end
+
+    def create
+        task = Task.find(params[:task_id])
+        @comment = task.comments.build(comment_params)
+        if @comment.save
+            redirect_to task_path(task), notice:'コメント追加'
+        else
+            flash.now[:error] = '更新出来ませんでした'
+            render :new
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
 end
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,4 +14,5 @@
 #
 class Comment < ApplicationRecord
     belongs_to :task
+    validates :content, presence:true
 end
