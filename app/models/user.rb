b5c3d1aa46ba0d6f42004e5a762951ece2efcaa4
daisuke_nabatame <nabanaba1225@gmail.com>
2020-11-20 00:18:50 +0900

    年齢を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one :profile, dependent: :destroy
 
-  delegate :birthday, :gender, to: :profile, allow_nil: true
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(board)
     boards.exists?(id: board.id)
